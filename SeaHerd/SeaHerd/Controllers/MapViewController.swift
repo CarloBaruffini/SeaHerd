764f0905b09214f9ab9c93bf2dfb16970673268c
Carlo Baruffini <ca.baruffini@gmail.com>
2019-10-19 15:38:46 +0200

    Ask for user location and implement clllocationmanager delegate

--- SeaHerd/SeaHerd/Controllers/MapViewController.swift
+++ SeaHerd/SeaHerd/Controllers/MapViewController.swift
@@ -12,25 +12,44 @@ import CoreLocation
 
 class MapViewController: UIViewController {
     
+    let locationManager = CLLocationManager()
     let jellyfish = MKPointAnnotation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+
+        mapView.delegate = self
+
+        if let coordinate = mapView.userLocation.location?.coordinate {
+            mapView.setCenter(coordinate, animated: true)
+        }
+        
         let location = CLLocationCoordinate2D(latitude: 32.026458, longitude: 32.026458)
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-        
-        self.mapView.setRegion(region, animated: true)
-        
         self.jellyfish.coordinate = location
         self.jellyfish.title = "Scusa, mi tocchi la medusa?"
         
         self.mapView.addAnnotation(self.jellyfish)
-
     }
     
-    
     @IBOutlet weak var mapView: MKMapView!
+}
+
+extension MapViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue: CLLocationCoordinate2D = manager.location!.coordinate
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        
+        self.mapView.setRegion(region, animated: true)
+    }
 }
